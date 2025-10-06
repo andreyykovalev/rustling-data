d8da20285a9e17dfe30aee215c39ce13e2af7ae6
andreykovalev <andreyykovalev@gmail.com>
2025-10-06 17:49:08 +0200

    potgres macro implement other crud methods; more examples

--- rustling-data/src/api.rs
+++ rustling-data/src/api.rs
@@ -1,8 +1,13 @@
 use mongodb::bson::oid::ObjectId;
+use sqlx::{Encode, Postgres, Type};
 
 #[async_trait::async_trait]
 pub trait Repository<T, ID> {
     async fn find_all(&self) -> Result<Vec<T>, anyhow::Error>;
+    async fn find_one(&self, id: &ID) -> Result<Option<T>, anyhow::Error>;
+    async fn insert_one(&self, entity: &T) -> Result<ID, anyhow::Error>;
+    async fn update_one(&self, id: &ID, entity: &T) -> Result<Option<T>, anyhow::Error>;
+    async fn delete_one(&self, id: &ID) -> Result<u64, anyhow::Error>;
 }
 
 #[async_trait::async_trait]
@@ -12,4 +17,14 @@ pub trait MongoRepository<E, ID> {
     async fn insert_one(&self, doc: &E) -> Result<ObjectId, anyhow::Error>;
     async fn update_one(&self, id: &ID, doc: &E) -> Result<Option<E>, anyhow::Error>;
     async fn delete_one(&self, id: &ID) -> Result<u64, anyhow::Error>;
+}
+
+
+pub trait Entity {
+    type Id;
+
+    fn columns() -> &'static [&'static str];
+
+    /// Return values generically, no dyn trait needed
+    fn values<'e>(&'e self) -> Vec<&'e (impl Encode<'e, Postgres> + Type<Postgres>)>;
 }
